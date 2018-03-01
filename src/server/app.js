8dbd31ac3b78c64f9c7003b19807b440ed6b448c
Danil Kazimirov <danil.kazim.99@gmail.com>
2018-03-01 02:58:59 +0200

    add: +package.json

---
+++ src/server/app.js
@@ -0,0 +1,43 @@
+// config
+require('dotenv').config();
+require('module-alias/register');
+// tools
+const services = require('@services');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const busboyBodyParser = require('busboy-body-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const logs = require('@logs')(module);
+const app = express();
+
+function init() {
+// run static file server
+    app.use(express.static(path.join(__dirname, 'public')));
+    // app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+    //don't show logs in test mode
+    if (process.env.NODE_ENV !== 'test') {
+        app.use(logger('combined')); //'combined' prints logs in apache style
+    }
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(busboyBodyParser());
+    app.use(cookieParser());
+// enable cors
+    var corsOption = {
+        origin: true,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+        exposedHeaders: ['x-auth-token']
+    };
+    app.use(cors(corsOption));
+    services.init(app);
+    app.use(require('@routes/index'));
+}
+
+init();
+
+module.exports = app;
---
+++ src/server/routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+const APIRouter = require('@API');
+const logs = require('@logs')(module);
+const path=require('path');
+// API processing
+router.use('/api/', APIRouter);
+
+// send root file
+router.use('*', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+});
+
+// catch 404 and forward to error handler
+router.use(function (req, res, next) {
+    logs.debug('Not fount URL: %s', req.url);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+// error handler
+router.use(function (err, req, res, next) {
+    logs.error('Internal error(%d): %s', res.statusCode, err.message);
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.json({
+        success: false,
+        message: err.message || "Server error",
+        code: err.status || 500
+    });
+});
+
+logs.info("configured");
+
+
+module.exports = router;
