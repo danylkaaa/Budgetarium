95de73084da2870b6971b835434ed5e98cd4c9e8
Danil Kazimirov <danil.kazim.99@gmail.com>
2018-03-11 00:22:16 +0200

    add tests order

--- test/index.js
+++ test/index.js
@@ -1,9 +1,3 @@
-// let mongoose = require("mongoose");
 require("module-alias/register");
-//Подключаем dev-dependencies
-let chai = require("chai");
-let chaiHttp = require("chai-http");
-// let server = require("../src/server/bin/www");
-chai.should();
-chai.use(chaiHttp);
-
+//Include tests
+require("routes/index");
