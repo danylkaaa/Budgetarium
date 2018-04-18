28a5543a34572a4aef30c01f5f0710644074dc31
Zulus <danil.kazim.99@gmail.com>
2018-04-18 03:10:11 +0300

    add controllers support for routing

--- src/server/controllers/index.ts
+++ src/server/controllers/index.ts
@@ -1,9 +1,8 @@
-import {Router, Application, Request, Response, NextFunction} from "express";
-import express from "express";
+import {Router, Request, Response, NextFunction} from "express";
+import * as express from "express";
 import APIController from "./api/index";
 import Logger from "@logger";
 import errorMiddleware from "@utils/errorMiddleware";
-import ServeError from "@errors/ServeError";
 import config from "@config";
 import path from "path";
 
@@ -12,7 +11,8 @@ const router: Router = express.Router();
 
 function error404(req: Request, res: Response, next: NextFunction): void {
     logs.debug("Not found URL: %s", req.url);
-    const err: ServeError = new ServeError("Not found", 404);
+    const err: any = new Error("Not found");
+    err.status = 404;
     next(err);
 }
 
@@ -27,16 +27,13 @@ function onError(err: any, req: Request, res: Response): void {
     });
 }
 
-
 function onSPA(req: Request, res: Response): void {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 }
 
-function connect(app: Application): void {
-    app.use("/api", APIController);
-    app.use("*", onSPA);
-    app.use(error404);
-    app.use(errorMiddleware, onError);
-}
+router.use("/api", APIController);
+router.use("*", onSPA);
+router.use(error404);
+router.use(errorMiddleware, onError);
 
 export default router;
--- src/server/services/errors/ServeError.ts
+++
@@ -1,7 +0,0 @@
-class ServeError extends Error {
-    constructor(public message: string, public status: number) {
-        super(message);
-    }
-}
-
-export default ServeError;
