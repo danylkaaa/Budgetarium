ba2566c222237f5428dadfdb245f16b0ebf5f556
Zulus <danil.kazim.99@gmail.com>
2018-04-18 04:40:25 +0300

    REMOVE test config DB URL

--- src/server/config/test.js
+++ src/server/config/test.js
@@ -1,9 +1,7 @@
 module.exports = {
     isDev: true,
     ROOT_URL: `localhost:${process.env.PORT}`,
-    DB_URL: process.env.DB_ADDRESS_TEST
-        .replace("<dbuser>", process.env.DB_USER_TEST)
-        .replace("<dbpassword>", process.env.DB_PSW_TEST),
+    DB_URL: "SOME_url",
     security: {
         TOKEN_SECRET_LENGTH: 10,
         tokenLife: {
