7b4a6d078e8e2b4e56ceada073177424c3e08ad1
Danil Kazimirov <danil.kazim.99@gmail.com>
2018-04-30 02:07:18 +0300

    seting up

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,22 +1,15 @@
-import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import * as React from "react";
+import "./App.css";
+import Button from "material-ui/Button";
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div>
+                <Button/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -1,11 +1,15 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+import {HashRouter} from "react-router-dom";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <HashRouter>
+        <App/>
+    </HashRouter>,
+    document.getElementById("root") as HTMLElement
 );
 registerServiceWorker();
