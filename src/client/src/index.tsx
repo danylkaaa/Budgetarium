c1b89a943077053e74b5ed03c8497a2a8debcb33
Danil Kazimirov <danil.kazim.99@gmail.com>
2018-04-30 05:03:26 +0300

    add persisted state and redux dev toll

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -2,13 +2,16 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
-import { store } from "@store/index";
+import { store,persistor } from "@store/index";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import Routes from "./Routes";
 ReactDOM.render(
     <Provider store={store}>
-        <Routes />
+        <PersistGate loading={null} persistor={persistor}>
+            <Routes />
+        </PersistGate>
     </Provider>,
     document.getElementById("root") as HTMLElement
 );
--- src/client/src/store/index.ts
+++ src/client/src/store/index.ts
@@ -1,12 +1,29 @@
 import { combineReducers, createStore, compose, applyMiddleware } from "redux";
 import createHistory from "history/createHashHistory";
-import {routerReducer, routerMiddleware } from "react-router-redux";
+import { routerReducer, routerMiddleware } from "react-router-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import localStorage from "redux-persist/lib/storage";
 
-const reducer = combineReducers({
+export const history = createHistory();
+
+const rootReducer = combineReducers({
     routing: routerReducer
 });
-export const history = createHistory();
-export const store = createStore(reducer, {}, compose(
-    applyMiddleware(routerMiddleware(history))
-));
+
+const persistConfig = {
+    key: "root",
+    storage: localStorage
+};
+
+
+const reducer = persistReducer(persistConfig, rootReducer);
+
+const composeEnhancers = (process.env.NODE_ENV !== "production") ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(routerMiddleware(history)),
+);
+
+export const store = createStore(reducer as any, {}, enhancer);
+export const persistor = persistStore(store);
 
